947e4ee9de22a925160825cfdfaaed3ac7a7e855
Sarah <sarah@whiteroom.work>
2023-06-19 12:01:37 +0800

    Console.log User model

--- src/utils/utils.test.ts
+++ src/utils/utils.test.ts
@@ -1,8 +1,14 @@
 // test for the functions that use the model go here
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
+
+import User from "../../db/models/user";
+
+// const User = require("../../db/models/user");
 
 describe("function", () => {
   describe("function name", () => {
-    it("should do something", () => {});
+    it("should do something", () => {
+      console.log("hello?", User); // okay, wasn't expecting it to return that but opkay
+    });
   });
 });
