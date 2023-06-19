f6ce33a2b3bdbd2d3235af567d4184df8bcbad36
Sarah <sarah@whiteroom.work>
2023-06-19 11:50:05 +0800

    Add unit test boilerplate for function

--- src/utils/utils.test.ts
+++ src/utils/utils.test.ts
@@ -1,1 +1,8 @@
 // test for the functions that use the model go here
+import { describe, expect, it } from "vitest";
+
+describe("function", () => {
+  describe("function name", () => {
+    it("should do something", () => {});
+  });
+});
