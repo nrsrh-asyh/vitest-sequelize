d880265d856f78021937622211f0bf10a01004df
Sarah <sarah@whiteroom.work>
2023-06-19 11:30:34 +0800

    Add test function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,1 +1,10 @@
 // functions that use the models go here
+
+// okay. how to write functions that use models?
+
+const User = require("../../db/models/user");
+const Admin = require("../../db/models/admin");
+
+export const testFunc = () => {
+  // how tf to use sequelize models
+};
