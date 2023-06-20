5296c11a823a91fd755f7295e4b0715fffd71302
Sarah <sarah@whiteroom.work>
2023-06-20 13:27:51 +0800

    Updated unit test

--- utils/utils.test.ts
+++ utils/utils.test.ts
@@ -1,17 +1,21 @@
 // tests for the functions go here
 import { describe, expect, it, test, vi } from "vitest";
+import { Admin } from "../db/models/admin";
+import { User } from "../db/models/user";
 import { adminFunc, userFunc } from "./utils";
 
 describe("utils", () => {
   describe.only("adminFunc", () => {
-    it("should do return all of the admins", async () => {
-      adminFunc();
+    it("should return all of the admins", async () => {
+      const admins = await adminFunc();
+      expect(admins).toBeInstanceOf(Admin);
     });
   });
 
   describe("userFunc", () => {
-    it("should do return all of the users", async () => {
-      userFunc();
+    it("should return all of the users", async () => {
+      const users = await userFunc();
+      expect(users).toBeInstanceOf(User);
     });
   });
 });
