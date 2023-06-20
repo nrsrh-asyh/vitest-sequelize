d6fc2a623e01dafc7b259fb9711c1906fa166974
Sarah <sarah@whiteroom.work>
2023-06-20 16:58:06 +0800

    Added sequelize-mock

--- utils/utils.test.ts
+++ utils/utils.test.ts
@@ -1,8 +1,36 @@
 // tests for the functions go here
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import { Admin } from "../db/models/admin";
 import { User } from "../db/models/user";
 import { adminFunc, userFunc } from "./utils";
+// import { SequelizeMock } from "sequelize-mock";
+
+// vi.mock("../db/models/admin", () => ({
+//   Admin: vi.fn(),
+// }));
+
+// const SequelizeMock = require("sequelize-mock");
+
+// const dbMock = SequelizeMock();
+
+// const mockAdmin = dbMock.define("Admin", {
+//   id: 2,
+//   firstName: "John",
+//   lastName: "Wick",
+// });
+
+// vi.mock("../db/models/admin.ts", () => {
+//   const Admin = vi.fn();
+//   Admin.prototype.findAll = mockAdmin;
+//   return { Admin };
+// });
+
+// vi.mock("../db/models/admin.ts", () => {
+//   const Admin = vi.fn(() => ({
+//     findAll: vi.fn(),
+//   }));
+//   return { Admin };
+// });
 
 describe("utils", () => {
   describe.only("adminFunc", () => {
