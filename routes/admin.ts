a12f0241583f75aa97d8e826542a8a7a9e0c4643
Sarah <sarah@whiteroom.work>
2023-06-20 14:47:21 +0800

    - Updated admin and user routes

--- routes/admin.ts
+++ routes/admin.ts
@@ -1,56 +1,18 @@
-import { router } from "express";
+// defining admin route
+import Fastify from "fastify";
 import { Admin } from "../db/models/admin";
 
-router.get("/", async (req, res, next) => {
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async (req, res) => {
   try {
     const admins = await Admin.findAll();
     res.send(admins);
-  } catch (error) {
-    next(error);
+  } catch (e) {
+    res.send(e);
   }
 });
 
-router.get("/:id", async (req, res, next) => {
-  try {
-    let admin = await Admin.findByPk(req.params.id);
-    if (admin) {
-      res.send(admin);
-    } else {
-      res.status(404).send("Admin not found");
-    }
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.post("/", async (req, res, next) => {
-  try {
-    let admin = await Admin.create(req.body);
-    res.status(201).send(admin);
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.put("/:id", async (req, res, next) => {
-  try {
-    let updateAdminInfo = await Admin.update(req.body, {
-      where: { id: req.params.id },
-      returning: true,
-    });
-    res.send(updateAdminInfo[1]);
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.delete("/:id", async (req, res, next) => {
-  try {
-    await Admin.destroy({ where: { id: req.params.id } });
-    res.status(204).send();
-  } catch (err) {
-    next(err);
-  }
-});
-
-module.exports = router;
+module.exports = fastify;
--- routes/user.ts
+++ routes/user.ts
@@ -1,26 +1,18 @@
-import { router } from "express";
+// defining user route
+import Fastify from "fastify";
 import { User } from "../db/models/user";
 
-router.get("/", async (req, res, next) => {
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async (req, res) => {
   try {
     const users = await User.findAll();
     res.send(users);
-  } catch (err) {
-    next(err);
+  } catch (e) {
+    res.send(e);
   }
 });
 
-router.get("/:id", async (req, res, next) => {
-  try {
-    let user = await User.findByPk(req.params.id);
-    if (user) {
-      res.send(user);
-    } else {
-      res.status(404).send("User not found");
-    }
-  } catch (err) {
-    next(err);
-  }
-});
-
-module.exports = router;
+module.exports = fastify;
